ff4548fd33fd1b2124be317d34e6f68a3e96b3be
y0m0 <lixi.daniele@gmail.com>
2019-09-08 19:48:25 +0200

    Add test for multiplier == 0

--- test_ex1.py
+++ test_ex1.py
@@ -1,7 +1,7 @@
 import pytest
 from ex1 import sum_of_multiples_divisible_by_3
 
-def test_with_limit_0():
-    limit = 0
-    m = 3
-    assert sum_of_multiples_divisible_by_3(limit, m) == 0
+def test_with_multiplier_0():
+    limit = 10
+    m = 0
+    assert sum_of_multiples_divisible_by_3(10, 0) == 0
--- test_generator.py
+++
@@ -1,7 +0,0 @@
-from ex1 import sum_of_multiples_div_by_3
-
-def test_with_limit_0():
-    limit = 0
-    m = 3
-    assert sum_of_multiples_div_by_3(limit, m) == 0
-
