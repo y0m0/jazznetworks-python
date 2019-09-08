df70fe6f2e0d763f58718c96e8a48288f1e5d047
y0m0 <lixi.daniele@gmail.com>
2019-09-08 20:22:55 +0200

    Test limit == 1 should return 0

--- test_ex1.py
+++ test_ex1.py
@@ -10,3 +10,8 @@ def test_with_limit_0():
     limit = 0
     m = 3
     assert sum_of_multiples_divisible_by_3(limit, m) == 0
+
+def test_with_limit_1():
+    limit = 1
+    m = 2
+    assert sum_of_multiples_divisible_by_3(limit, m) == 0
