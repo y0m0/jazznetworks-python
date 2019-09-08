679e273729cb40aa2a129a053600134b2d520f2c
y0m0 <lixi.daniele@gmail.com>
2019-09-08 19:53:12 +0200

    Add test for limit == 0

--- test_ex1.py
+++ test_ex1.py
@@ -5,3 +5,8 @@ def test_with_multiplier_0():
     limit = 10
     m = 0
     assert sum_of_multiples_divisible_by_3(10, 0) == 0
+
+def test_with_limit_0():
+    limit = 0
+    m = 3
+    assert sum_of_multiples_divisible_by_3(limit, m) == 0
