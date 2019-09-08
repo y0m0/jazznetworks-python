9972be3c1ace88d33f9946e56963dea968f3d767
y0m0 <lixi.daniele@gmail.com>
2019-09-08 19:47:54 +0200

    Add test to check for

--- test_ex1.py
+++ test_ex1.py
@@ -1,7 +1,7 @@
-from ex1 import sum_of_multiples_div_by_3
+import pytest
+from ex1 import sum_of_multiples_divisible_by_3
 
 def test_with_limit_0():
     limit = 0
     m = 3
-    assert sum_of_multiples_div_by_3(limit, m) == 0
-
+    assert sum_of_multiples_divisible_by_3(limit, m) == 0
