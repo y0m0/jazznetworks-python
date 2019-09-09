3f19e2933a7113a6448356136864a838b849cead
y0m0 <lixi.daniele@gmail.com>
2019-09-09 12:26:41 +0200

    return list of consecutive numbers up to n, all initial test passed

--- ex2.py
+++ ex2.py
@@ -3,3 +3,5 @@ def consecutive_numbers(n: int) -> list:
     Returns a list of lists of incremental numbers up to n in the format:
     [[1], [1, 2], [1, 2, n]]
     """
+
+    return [list(range(1, x + 1)) for x in range(1, n + 1)]
